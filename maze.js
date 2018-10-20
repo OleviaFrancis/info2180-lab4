50d2bac7c3cc8f9dccfc932f3210af8000c7431e
Olevia Francis <quetysha@gmail.com>
2018-10-19 19:58:46 -0500

    Add exercise 5

--- maze.js
+++ maze.js
@@ -4,6 +4,9 @@ let starter = false;
 window.onload=function(){
 	let x = document.getElementById("boundary1");
 	let y = document.querySelectorAll(".boundary");
+	let status = document.getElementById("status");
+	let maze = document.getElementById("maze");
+	let end= document.getElementById("end");
 
 	function changeBoundary1(){
 		y.forEach(function(item,index){
@@ -18,7 +21,31 @@ window.onload=function(){
 		});
 	}
 
-	
+	function start(){
+		let start = document.getElementById("start");
+		start.addEventListener("click",function(){
+			status.innerHTML = "Move your mouse over the \"S\" to begin.";
+			let x = document.getElementById("boundary1");
+			starter = true;
+			if  (red === true){
+				
+				let x = document.getElementById("boundary1");
+				let y = document.querySelectorAll(".boundary") ;
+				y.forEach(function(item,index){
+					item.classList.remove("youlose");
+					red = false;
+				});	
+			}
+		});
+	}
+
+	function ender(){
+		end.addEventListener("mouseover",()=>{
+			if (red === false){
+				status.innerHTML = "You Win";
+			}
+		});
+	}
 
 	function changeColor(){
 		y.forEach(function(item,index){
@@ -28,8 +55,7 @@ window.onload=function(){
 	}
 
 	changeBoundary1();
-	ifLeaveArea();
-	
-
+	start();
+	ender();
 
 }
